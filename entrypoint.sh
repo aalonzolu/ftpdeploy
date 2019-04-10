ee49c0238dc13f8c025c4ae6b9937779bdb90adf
Andrés Alonzo y Alonzo <5524124+aalonzolu@users.noreply.github.com>
2019-04-09 22:28:53 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,4 @@ set ftp:ssl-allow off
 open $1
 user $2 $3
 lcd $4
-mirror --reverse --delete --verbose $4 $5 --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/
-bye"
+mirror --reverse --delete --verbose $4 $5 --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
